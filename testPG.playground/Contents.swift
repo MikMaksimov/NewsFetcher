750301e3d7f712c1c8ceabbec9553837c0c51224
MikMaksimov <36266447+MikMaksimov@users.noreply.github.com>
2023-04-02 12:31:08 +0300

    Delete not used directory

--- testPG.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")!
-Task {
-    let (data, response) = try await URLSession.shared.data(from: url)
-    if let httpResponse = response as? HTTPURLResponse,
-        httpResponse.statusCode == 200,
-        let string = String(data: data, encoding: .utf8) {
-            print(string)
-    }
-}
